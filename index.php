fc8fe2cef2911cfc29bd9d9f0ecf501a3d23907f
IgorPatrykPraktyki <115373289+Damiana214155255@users.noreply.github.com>
2023-02-08 12:10:24 +0100

    a

--- index.php
+++ index.php
@@ -29,17 +29,17 @@
       <th>Password</th>
       </tr>
       <?php
-$conn = mysqli_connect("localhost", "root", "", "testttt");
+$conn = mysqli_connect("localhost", "root", "", "tygodniowy_harmonogram_pracy");
    if ($conn-> connect_error) {
       die("Connection failed:". $conn-> connect_error);
    }
 
-   $sql = "SELECT id, username, password from login";
+   $sql = "SELECT data, imie from tyhoharmonogrampracy";
    $result = $conn-> query($sql);
 
    if ($result-> num_rows > 0) {
       while ($row = $result-> fetch_assoc()) {
-        echo "<tr><td>". $row["id"] . "</td><td>" . $row["username"] . "<td><td>" . $row["password"] . "</td></tr>";
+        echo $row["data"] . "<td><td>" . $row["imie"] . "</td></tr>";
       }
       echo "</table>";
    }
